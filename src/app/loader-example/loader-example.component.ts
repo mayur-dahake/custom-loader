749cdcf2ebdc86f43c21029801076ed4529921cf
Rachanadahake <rachanadahake21@gmail.com>
2025-05-11 21:54:15 +0530

    Accordian added

--- src/app/loader-example/loader-example.component.ts
+++ src/app/loader-example/loader-example.component.ts
@@ -5,11 +5,10 @@ import { LoaderType } from '../loader-type';
   selector: 'app-loader-example',
   standalone: false,
   templateUrl: './loader-example.component.html',
-  styleUrl: './loader-example.component.scss'
+  styleUrl: './loader-example.component.scss',
 })
 export class LoaderExampleComponent {
-
-loaders: LoaderType[] = [
+  loaders: LoaderType[] = [
     'spinner-cutout',
     'spinner-border',
     'spinner-multiring',
@@ -25,4 +24,44 @@ loaders: LoaderType[] = [
 
     // ... add more as needed
   ];
+
+  loaderGroups = [
+    {
+      name: 'Spinners',
+      open: true,
+      loaders: [
+        'spinner-cutout',
+        'spinner-border',
+        'spinner-multiring',
+        'spinner-rotating-dot',
+        'spinner-segmented-ring',
+        'spinner-angled-arc',
+        'spinner-nested-circle',
+        'spinner-orbiting-dot',
+        'spinner-gear',
+        'spinner-stepped-wheel',
+        'spinner-rotating-matrix',
+        'spinner-circular-fade',
+      ],
+    },
+    {
+      name: 'Progress Bars',
+      open: false,
+      loaders: ['bar', 'pulse'],
+    },
+    {
+      name: 'Dots',
+      open: false,
+      loaders: ['dots'],
+    },
+    {
+      name: 'Classic',
+      open: false,
+      loaders: ['dots'],
+    },
+  ];
+
+  toggleAccordion(group: any) {
+    group.open = !group.open;
+  }
 }
